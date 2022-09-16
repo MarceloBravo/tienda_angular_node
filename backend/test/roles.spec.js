8238815a5b3d7f9bfb5582ecafa948248ae4323d
Marcelo <mabc@live.cl>
2022-09-16 20:21:46 -0300

    Se escribieron los test para los endpoints de roles, usuarios y login

---
+++ backend/test/roles.spec.js
@@ -0,0 +1,167 @@
+import supertest from 'supertest'
+import app from '../src/index.js'
+import { server } from '../src/index.js'
+import { RolesModel } from '../models/RolModel.js'
+import { UsuarioModel } from '../models/UsuarioModel.js'
+import { rolesData, credentials, usuarioTesting, rolTesting } from './helpers.js'
+
+const api = supertest(app)
+let token = ""
+
+describe('Test para los endpoints de roles',  () => {
+
+    beforeAll(async () => {
+        await UsuarioModel.destroy({truncate: true, cascade: true, restartIdentity: true})
+        await RolesModel.destroy({truncate: true, cascade: true, restartIdentity: true})
+        
+        //Creando el rol y el usuario para loguearse durante las pruebas de testing
+        const _rolTesting = await RolesModel.create(rolTesting)
+        _rolTesting.save()
+
+        const _usuarioTesting = await UsuarioModel.create(usuarioTesting)
+        _usuarioTesting.save()
+
+        //Logueandose para los testings
+        const res = await api.post('/login')
+            .send(credentials)
+        token = res.body.access_token
+    })
+
+
+    beforeEach(async ()=>{
+        await RolesModel.destroy({ truncate : true, cascade: true, restartIdentity: true })
+        
+        const _rol1 = await RolesModel.create(rolesData[0])        
+        _rol1.save()
+
+        const _rol2 = await RolesModel.create(rolesData[1])
+        _rol2.save()        
+    })
+
+    
+    it('Crea un rol "POST /roles"', async () => {
+        const rol = {
+            nombre: "Admin",
+            descripcion: "Rol para el usuario administrador del sistema"
+        }
+        const res = await api            
+            .post('/roles')
+            .auth(token, {type: 'bearer'})
+            .send(rol)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        expect(res.body.data.descripcion).toEqual(rol.descripcion)
+
+    })
+    
+
+    it('Actualiza un rol "PUT /roles/:id"', async () => {
+        const res1 = await api        
+        .get('/roles/1')
+        .auth(token, {type: 'bearer'})
+
+        expect(res1.body.data.nombre).toEqual('Rol test 1')
+
+        let rol = res1.body.data
+        rol.nombre = 'Invitado'
+        rol.descripcion = 'El rol ha sido actualizado a invitado'
+        
+        const res2 = await api                
+                .put('/roles/1')
+                .auth(token, {type: 'bearer'})
+                .send(rol)
+                .expect(200)
+                .expect('Content-Type', /application\/json/)
+
+        expect(res2.body.data.nombre).toEqual(rol.nombre)
+    })
+    
+
+    it('Retorna el listado de roles "GET /roles"', async () => {
+        const res = await api        
+        .get('/roles/pag/1')
+        .auth(token, {type: 'bearer'})
+
+
+        expect(res.status).toEqual(200);
+        expect(res.type).toEqual(expect.stringContaining('json'));
+        expect(res.body.count).toEqual(rolesData.length)
+    })
+
+
+    it('Retorna el rol con el id especificado "GET /roles:id"', async ()=> {
+        const res = await api        
+        .get('/roles/1')
+        .auth(token, {type: 'bearer'})
+
+        expect(res.status).toEqual(200);
+        expect(res.type).toEqual(expect.stringContaining('json'));
+        expect(res.body.data.nombre).toEqual('Rol test 1')
+    })
+
+    
+    it('Filtra un registro a partir de un texto buscado "GET /roles/filtrar/:texto/:pag"', async ()=>{
+        const res = await api        
+        .get('/roles/filtrar/N°1/1')
+        .auth(token, {type: 'bearer'})
+
+        expect(res.status).toEqual(200)
+        expect(res.type).toEqual(expect.stringContaining('json'))
+        expect(res.body.count).toEqual(1)
+    })
+
+    
+    it('Retorna todos los roles "GET /roles/get/all"', async ()=> {
+        const res = await api        
+        .get('/roles/get/all')
+        .auth(token, {type: 'bearer'})
+
+        expect(res.status).toEqual(200)
+        expect(res.body.length).toEqual(rolesData.length)
+        expect(res.body[1].descripcion).toContain('N°2')
+        
+        //expect(res.body.)
+    })
+
+
+    it('Elimina un registro de la tabla de roles "DELETE /roles/:id"', async ()=>{
+        const res = await api        
+        .delete('/roles/1')
+        .auth(token, {type: 'bearer'})
+
+        expect(res.status).toEqual(200)
+        expect(res.body.mensaje).toEqual('El rol ha sido eliminado exitosamente.')
+
+        const res2 = await api        
+        .delete('/roles/100')
+        .auth(token, {type: 'bearer'})
+
+        expect(res2.status).toEqual(200)
+        expect(res2.body.mensaje).toEqual('El rol no fue encontrado o no existe.')
+    })
+
+
+    it('Marca un registro como borrado en la tabla de roles "DELETE /roles/softdelete/:id"', async ()=>{
+        const res = await api        
+        .delete('/roles/softdelete/1')
+        .auth(token, {type: 'bearer'})
+
+        expect(res.status).toEqual(200)
+        expect(res.body.mensaje).toEqual('El rol ha sido borrado exitosamente.')
+
+        const res2 = await api        
+        .delete('/roles/softdelete/1')
+        .auth(token, {type: 'bearer'})
+
+        expect(res2.status).toEqual(404)
+        expect(res2.body.mensaje).toEqual('Imposible eliminar. Registro no existe o no fue encontrado.')
+    })
+
+    
+    afterAll(()=>{
+        //console.log('SERVER afterEach', server)
+        if(server) server.close()
+    })
+    
+})
